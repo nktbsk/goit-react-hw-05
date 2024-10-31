4f9902e100bb8a72fba5b920540f84b742664edb
nktbsk <nikita.bashkir01@gmail.com>
2024-10-31 10:59:09 +0200

    Add search movies

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,16 +3,15 @@ import { useState, useEffect } from "react";
 import style from "./MovieDetailsPage.module.css";
 import axios from "axios";
 
-const MovieDetailsPage = ({ movies }) => {
-  const [genres, setGenres] = useState([]);
+const MovieDetailsPage = () => {
+  const [movie, setMovie] = useState(null);
   const { movieId } = useParams();
   const navigate = useNavigate();
-  const movie = movies.find((movie) => movie.id === parseInt(movieId));
 
-  // Запрос для списка жанров
   useEffect(() => {
+    // Запрос на детальную информацию о фильме
     axios
-      .get("https://api.themoviedb.org/3/genre/movie/list", {
+      .get(`https://api.themoviedb.org/3/movie/${movieId}`, {
         headers: {
           Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZWZmZDczNTgyNWRkZjM5MDBjMDZlOGJjZjhmMTlmZSIsIm5iZiI6MTczMDI4MjQ2Ny4wODY0OTA5LCJzdWIiOiI2NzE4YWMyOTI3YmQ1N2Q5MWY2MjFjZWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.qX7aLZ_Vb3dMm60DhbteGJHM1Hfppq8N2FFagwWRIdU`,
         },
@@ -21,21 +20,15 @@ const MovieDetailsPage = ({ movies }) => {
         },
       })
       .then((response) => {
-        setGenres(response.data.genres);
+        setMovie(response.data);
       })
       .catch((error) => {
-        console.error("Error fetching genres:", error);
+        console.error("Ошибка при загрузке фильма:", error);
       });
-  }, []);
-
-  const getGenreNames = (genreIds) => {
-    return genreIds.map(
-      (id) => genres.find((genre) => genre.id === id)?.name || "Unknown"
-    );
-  };
+  }, [movieId]);
 
   if (!movie) {
-    return <p>Фильм не найден.</p>; // Сообщение, если фильм не найден
+    return <p>Фильм не найден.</p>;
   }
 
   return (
@@ -44,7 +37,6 @@ const MovieDetailsPage = ({ movies }) => {
         Go back
       </button>
       <h1>{movie.title}</h1>
-
       <div className={style.wrapper}>
         <img
           className={style.img}
@@ -55,7 +47,7 @@ const MovieDetailsPage = ({ movies }) => {
           <p>{movie.overview}</p>
           <p>Rating: {movie.vote_average}</p>
           <p>Release date: {movie.release_date}</p>
-          <p>Genres: {getGenreNames(movie.genre_ids).join(", ")}</p>
+          <p>Genres: {movie.genres.map((genre) => genre.name).join(", ")}</p>
           <p>Original language: {movie.original_language}</p>
         </div>
       </div>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,10 +1,63 @@
-const MoviePage = () => {
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const MoviesPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState(searchParams.get("query") || "");
+
+  useEffect(() => {
+    if (query) {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZWZmZDczNTgyNWRkZjM5MDBjMDZlOGJjZjhmMTlmZSIsIm5iZiI6MTczMDI4MjQ2Ny4wODY0OTA5LCJzdWIiOiI2NzE4YWMyOTI3YmQ1N2Q5MWY2MjFjZWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.qX7aLZ_Vb3dMm60DhbteGJHM1Hfppq8N2FFagwWRIdU`,
+          },
+          params: {
+            query: query,
+            language: "en-US",
+            page: 1,
+          },
+        })
+        .then((response) => {
+          setMovies(response.data.results);
+        })
+        .catch((error) => {
+          console.error("Ошибка при поиске:", error);
+        });
+    }
+  }, [query]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const query = e.target.elements.searchInput.value;
+    setSearchParams({ query });
+    setQuery(query);
+  };
+
   return (
     <div>
-      <input type="text" />
-      <button>Search</button>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          name="searchInput"
+          placeholder="Поиск фильмов..."
+          defaultValue={query}
+        />
+        <button type="submit">Искать</button>
+      </form>
+
+      <div>
+        {movies.map((movie) => (
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            {movie.title}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default MoviePage;
+export default MoviesPage;
